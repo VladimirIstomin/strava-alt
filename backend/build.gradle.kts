ab25c3ef7d830151ee596fcbc21a9d73977b3bb8
VladimirIstomin <69541514+VladimirIstomin@users.noreply.github.com>
2025-06-29 21:56:25 +0300

    Enable CORS for frontend

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation:2.3.1")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.1")
     implementation("io.ktor:ktor-server-sessions:2.3.1")
+    implementation("io.ktor:ktor-server-cors:2.3.1")
     implementation("io.ktor:ktor-client-core:2.3.1")
     implementation("io.ktor:ktor-client-cio:2.3.1")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.1")
